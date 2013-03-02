020fc4e2715e64a4b67fe410d68bd841fa297fe2
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2013-03-02 16:39:47 +0100

    Added tests for the `SerialConnection`.

--- test/connections/SerialConnection.js
+++ test/connections/SerialConnection.js
@@ -1,3 +1,4 @@
+/*jshint maxlen:999*/
 /*global describe:false,it:false*/
 
 'use strict';
@@ -5,14 +6,88 @@
 require('should');
 
 var LIB_DIR = process.env.LIB_FOR_TESTS_DIR || '../../lib';
+var EventEmitter = require('events').EventEmitter;
 var SerialConnection = require(LIB_DIR + '/connections/SerialConnection');
 
 describe("SerialConnection", function()
 {
-  it("should ", function()
+  it("should emit error events emitted by the specified SerialPort", function()
   {
+    var serialPort = new EventEmitter();
+    var expectedHits = [[new Error("ERROR!")]];
+    var actualHits = [];
 
+    var conn = new SerialConnection(serialPort);
+
+    conn.on('error', function()
+    {
+      actualHits.push(arguments);
+    });
+
+    serialPort.emit('error', expectedHits[0][0]);
+
+    actualHits.should.be.eql(expectedHits);
   });
 
+  it("should emit data events emitted by the specified SerialPort", function()
+  {
+    var serialPort = new EventEmitter();
+    var expectedHits = [[new Buffer(10)]];
+    var actualHits = [];
 
+    var conn = new SerialConnection(serialPort);
+
+    conn.on('data', function()
+    {
+      actualHits.push(arguments);
+    });
+
+    serialPort.emit('data', expectedHits[0][0]);
+
+    actualHits.should.be.eql(expectedHits);
+  });
+
+  describe("write", function()
+  {
+    it("should delegate to a write method of the specified SerialPort", function()
+    {
+      var serialPort = new EventEmitter();
+      var expectedHits = [[new Buffer(10)]];
+      var actualHits = [];
+
+      serialPort.write = function()
+      {
+        actualHits.push(arguments);
+      };
+
+      var conn = new SerialConnection(serialPort);
+
+      conn.write(expectedHits[0][0]);
+
+      actualHits.should.be.eql(expectedHits);
+    });
+
+    it("should emit exceptions thrown by the SerialPort.write() as error events", function()
+    {
+      var serialPort = new EventEmitter();
+      var expectedError = new Error();
+      var actualError = null;
+
+      serialPort.write = function()
+      {
+        throw expectedError;
+      };
+
+      var conn = new SerialConnection(serialPort);
+
+      conn.on('error', function(err)
+      {
+        actualError = err;
+      });
+
+      conn.write(new Buffer(10));
+
+      actualError.should.be.equal(expectedError);
+    });
+  });
 });
